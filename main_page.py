340546587a3d28bb2865821a319caa692847d244
virenn <virenn9999@gmail.com>
2023-10-22 00:08:13 +0530

    components and layout fixed

---
+++ main_page.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from components import st_card,colored_headings
+
+st.write("# Models")
+
+
+colored_headings("Tabular Models",heading_level=2,color="FF6AC2")
+
+
+columns = st.columns(3)
+
+for i, col in enumerate(columns):
+
+    with col:
+        h3_title = f"Tabular Model_{i}"
+        content = f"Model_{i}.\nOkay, lets face it. Nothing can come between you and I."
+        url = "https://www.youtube.com/watch?v=S3IQwuYX_ls"
+        st_card(h3_content=h3_title,content=content,button_link=url)
+
+
+
+st.write("## Computer Vision Models")
+colored_headings("Computer Vision Models",heading_level=2,color="FF6AC2")
+
+columns = st.columns(3)
+
+for i, col in enumerate(columns):
+
+    with col:
+        h3_title = f"Model_{i}"
+        content = f"CV Model_{i}.\nOkay, lets face it. Nothing can come between you and I."
+        url = "https://www.youtube.com/watch?v=S3IQwuYX_ls"
+        st_card(h3_content=h3_title,content=content,button_link=url)
