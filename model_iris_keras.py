cb46b4d8c5d8a1c4b82c76b7d2e503d757a46ed4
virenn <virenn9999@gmail.com>
2023-11-01 17:26:20 +0530

    changed path of keras model

--- model_iris_keras.py
+++ model_iris_keras.py
@@ -30,7 +30,7 @@ class IrisKeras():
         """
         self.architecture = self.root_dir/ 'media/iris_keras.png'
         self.cometml_url = "https://www.comet.com/wereign/solar-detection-v2/view/EjM3aobkDhccBouofxikIQtrc/panels"
-        self.model = keras.models.load_model( self.root_dir / 'saved_models/iris_model.h5')
+        self.model = keras.models.load_model('./iris_model.keras')
 
 
     def inference(self,sep_len,sep_wid,pet_len,pet_wid):
